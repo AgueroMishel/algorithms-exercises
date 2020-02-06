db557e30dc36ba128787e10015a1cb9edaab4080
Mishel Agüero <m.aguero@outlook.com>
2020-02-06 12:25:48 -0600

    Adding logic to solve this problem

--- src/main/java/com/itisol/number/ConvertNumber.java
+++ src/main/java/com/itisol/number/ConvertNumber.java
@@ -3,6 +3,20 @@ package com.itisol.number;
 public class ConvertNumber {
 
     public static String convertNumberToCommaFormattedString(int number) {
-        return null;
+        int currentDigits;
+        String formattedNumber = "";
+
+        while(number > 0) {
+            if(number > 999) {
+                currentDigits = number % 1000;
+                formattedNumber = "," + String.format("%03d", currentDigits) + formattedNumber;
+            } else {
+                formattedNumber = number + formattedNumber;
+            }
+
+            number = number / 1000;
+        }
+
+        return formattedNumber;
     }
 }
